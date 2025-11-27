cb4294c6f7d4b186f7bae76928dd145d6ee0f88b
Matheus <matheus.moreira2172@gmail.com>
2025-11-26 21:59:56 -0300

    Falta de certificado não causará mais erro

--- vite.config.ts
+++ vite.config.ts
@@ -4,26 +4,37 @@ import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 import fs from "fs";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    https: {
-      key: fs.readFileSync(path.resolve(__dirname, "certs/localhost-key.pem")),
-      cert: fs.readFileSync(path.resolve(__dirname, "certs/localhost.pem")),
+export default defineConfig(({ mode }) => {
+  const keyPath = path.resolve(__dirname, "certs/localhost-key.pem");
+  const certPath = path.resolve(__dirname, "certs/localhost.pem");
+
+  const isDevWithCerts =
+    mode === "development" && fs.existsSync(keyPath) && fs.existsSync(certPath);
+
+  return {
+    plugins: [react(), tailwindcss()],
+    server: {
+      https: isDevWithCerts
+        ? {
+            key: fs.readFileSync(keyPath),
+            cert: fs.readFileSync(certPath),
+          }
+        : undefined,
+
+      proxy: {
+        "/api": {
+          target: "https://localhost:8080/",
+          secure: false,
+          changeOrigin: true,
+        },
+      },
+      host: "localhost",
+      port: 5173,
     },
-    proxy: {
-      "/api": {
-        target: "https://localhost:8080/",
-        secure: false,
-        changeOrigin: true,
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
       },
     },
-    host: "localhost",
-    port: 5173,
-  },
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
+  };
 });
